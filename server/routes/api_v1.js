237788896bbee8fa40c63bbe7604f4edabf20c86
Tyler Walker <twalkerweb@gmail.com>
2018-01-24 14:39:25 -0800

    removing garbage and updating the readme

--- server/routes/api_v1.js
+++ server/routes/api_v1.js
@@ -9,10 +9,17 @@ import { Router } from 'express';
 const router = Router();
 
 router.route('/info').get(getInfo);
+router.route('/parks').get(getParks);
 
 function getInfo(req, res) {
   res.status(200).json({
     message: 'GET INFO'
+  });
+}
+
+function getParks(req, res) {
+  res.status(200).json({
+    message: 'Parks'
   });
 }
 
